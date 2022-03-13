cf28a561b23c264fa7fc980f0b5c9f6e80133f0a
Oleg Fedorov <olegajaro94@gmail.com>
2022-03-13 12:22:43 +0300

    setup: Create Xcode project without storyboards

---
+++ StarbucksHomeScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarbucksHomeScreen
+//
+//  Created by Олег Федоров on 13.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
