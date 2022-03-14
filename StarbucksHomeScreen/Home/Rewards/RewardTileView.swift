a338656fafe9c9121665ee147441d1deefcf0097
Oleg Fedorov <olegajaro94@gmail.com>
2022-03-14 17:18:27 +0300

    feat: Adding the BalanceView

--- StarbucksHomeScreen/Home/Rewards/RewardTileView.swift
+++ StarbucksHomeScreen/Home/Rewards/RewardTileView.swift
@@ -9,7 +9,38 @@ import UIKit
 
 class RewardTileView: UIView {
     
+    let balanceView = BalanceView()
+    
     override var intrinsicContentSize: CGSize {
         return CGSize(width: 100, height: 300)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension RewardTileView {
+    private func style() {
+        balanceView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        addSubview(balanceView)
+        
+        NSLayoutConstraint.activate([
+            balanceView.topAnchor.constraint(equalTo: topAnchor),
+            balanceView.leadingAnchor.constraint(
+                equalToSystemSpacingAfter: leadingAnchor,
+                multiplier: 2
+            )
+        ])
     }
 }
