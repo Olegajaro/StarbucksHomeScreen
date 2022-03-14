697376a68cd48d5f5dc3e9dab44fd41319b39e00
Oleg Fedorov <olegajaro94@gmail.com>
2022-03-14 10:51:55 +0300

    refactor: Clean up home viewController

--- StarbucksHomeScreen/Home/HomeViewController.swift
+++ StarbucksHomeScreen/Home/HomeViewController.swift
@@ -12,10 +12,7 @@ class HomeViewController: StarbucksViewController {
     // MARK: - UIElements
     let headerView = HomeHeaderView()
     var headerViewTopConstraint: NSLayoutConstraint?
-    
-    let tableView = UITableView()
-    
-    let cellID = "cellID"
+        
     let titles = [
         "Star balance",
         "Bonus stars",
@@ -29,7 +26,6 @@ class HomeViewController: StarbucksViewController {
         super.viewDidLoad()
         
         setupNavBar()
-        setupTableView()
         setupTabBarImage(imageName: "house.fill", title: "Home")
         style()
         layout()
@@ -39,14 +35,6 @@ class HomeViewController: StarbucksViewController {
     private func setupNavBar() {
         navigationController?.navigationBar.topItem?.title = "Good afternoon, Oleg ☀️"
     }
-    
-    private func setupTableView() {
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
-        tableView.sectionHeaderTopPadding = 0
-    }
 }
 
 // MARK: - Style and Layout function
@@ -54,13 +42,10 @@ extension HomeViewController {
     private func style() {
         headerView.translatesAutoresizingMaskIntoConstraints = false
         headerView.backgroundColor = .systemBackground
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private func layout() {
         view.addSubview(headerView)
-        view.addSubview(tableView)
         
         // headerView constraint
         headerViewTopConstraint = headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
@@ -70,74 +55,24 @@ extension HomeViewController {
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
-        
-        // tableView constraint
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
     }
 }
+//    // Collapsible header
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let y = scrollView.contentOffset.y
+//
+//        let swipingDown = y <= 0
+//        let shouldSnap = y > 30
+//        let labelHeight = headerView.greeting.frame.height + 16 // label + spacer (102)
+//
+//        UIView.animate(withDuration: 0.3) {
+//            self.headerView.greeting.alpha = swipingDown ? 1.0 : 0.0
+//        }
+//
+//        UIViewPropertyAnimator.runningPropertyAnimator(
+//            withDuration: 0.3, delay: 0, options: []) {
+//                self.headerViewTopConstraint?.constant = shouldSnap ? -labelHeight : 0
+//                self.view.layoutIfNeeded()
+//            }
+//    }
 
-// MARK: - UITableViewDataSource
-extension HomeViewController: UITableViewDataSource {
-    func tableView(
-        _ tableView: UITableView,
-        numberOfRowsInSection section: Int
-    ) -> Int {
-        titles.count
-    }
-    
-    func tableView(
-        _ tableView: UITableView,
-        cellForRowAt indexPath: IndexPath
-    ) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: cellID, for: indexPath
-        )
-        
-        cell.textLabel?.text = titles[indexPath.row]
-        cell.accessoryType = .disclosureIndicator
-        
-        return cell
-    }
-}
-
-// MARK: - UITableViewDelegate
-extension HomeViewController: UITableViewDelegate {
-    func tableView(
-        _ tableView: UITableView,
-        didSelectRowAt indexPath: IndexPath
-    ) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    func tableView(
-        _ tableView: UITableView,
-        heightForRowAt indexPath: IndexPath
-    ) -> CGFloat {
-        300
-    }
-    
-    // Collapsible header
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let y = scrollView.contentOffset.y
-        
-        let swipingDown = y <= 0
-        let shouldSnap = y > 30
-        let labelHeight = headerView.greeting.frame.height + 16 // label + spacer (102)
-        
-        UIView.animate(withDuration: 0.3) {
-            self.headerView.greeting.alpha = swipingDown ? 1.0 : 0.0
-        }
-        
-        UIViewPropertyAnimator.runningPropertyAnimator(
-            withDuration: 0.3, delay: 0, options: []) {
-                self.headerViewTopConstraint?.constant = shouldSnap ? -labelHeight : 0
-                self.view.layoutIfNeeded()
-            }
-    }
-}
